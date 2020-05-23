cf4992bc8d58241b4964c0250e0dea8831685a14
Robert-Murdoch <rmurdoc90@gmail.com>
2020-05-23 08:55:26 -0700

    created React app, installed axios, react-router-dom and styled component dependencies. creating components folder along with components, created utils folder with axiosWithAuth file

---
+++ sleep-tracker/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import './App.css';
+import styled from 'styled-components';
+import PrivateRoute from './components/PrivateRoute';
+import SleepInput from './components/SleepInput';
+
+ const AppSection = styled.section`
+    border:2px solid back;
+    background-color:dodgerblue;
+ `;
+
+function App() {
+  return (
+  <Router>
+    <div className="App">
+      <AppSection>  {/* Styling section for the full application */}
+        <NavBar/>
+          <Switch>
+            {/* Switch allows for navigating through path without having to rerender the entire page, makes for cleaner more astetically pleasing user experience */}
+
+            <PrivateRoute exact path='/sleepdata' component={SleepData}/>
+            <PrivateRoute exact path='/sleepinput' component={SleepInput}/>
+            {/* Private Routes are end points/pages restricted to only verfiied users with tokens through utils-axiosWithAuth */}
+            
+            <Route path='/login' component={LoginPage}/>
+            <Route path='/' component={LoginPage}/>
+            <Route path='/signup' component={SignUpPage}/>
+
+          </Switch>
+      </AppSection>
+    
+    </div>
+  </Router>
+  );
+}
+
+export default App;
---
+++ sleep-tracker/src/components/SleepInput.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const SleepInput = () => {
+    const [sleepInput, setSleepInput] = useState()
+    
+    return(
+        <div>
+            <h1>Sleep Input: Test</h1>
+        </div>
+    )
+}
+
+export default SleepInput
