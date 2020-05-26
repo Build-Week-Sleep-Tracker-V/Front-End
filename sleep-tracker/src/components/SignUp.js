24f8646d0086e4e429dcedcf41cf963b59dc9c8b
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-05-26 10:13:40 -0700

    signup form

--- sleep-tracker/src/components/SignUp.js
+++ sleep-tracker/src/components/SignUp.js
@@ -5,11 +5,17 @@ import {useHistory} from 'react-router-dom';
 const SignUp = () => {
     const history = useHistory()
     const [signUpData, setSignUpData] = useState({
-        firstName:''
+        userName:'',
+        password: '',
+        verify: ''
         // React - fill in properties needed for log in form that will be used for state management //
     })
 
-        // React - onChange function to handle form changes //
+    const onInputChange = evt=>{
+        const [name] = evt.target
+        const[value] = evt.target
+        setSignUpData({...signUpData, [name]: value})
+    }
 
 
     const submit = e =>{ 
@@ -27,11 +33,23 @@ const SignUp = () => {
             })
     }
     return(
-        <div>
+       <form>
+       <div>
             <h1>Sign Up</h1>
+            <h1>Login</h1>
+                <label>Username:
+                    <input type='text' placeholder='Create a username' maxLength='100' name='userName' value={signUpData.userName} onChange={onInputChange} />
+                </label>
+                <label>Password:
+                    <input type='text' placeholder='Create a password' maxLength='100' name='password' value={signUpData.password} onChange={onInputChange} />
+                </label>
+                <label>Verify password:
+                    <input type='text' placeholder='Retype your password' maxLength='100' name='verify' value={signUpData.verify} onChange={onInputChange} />
+                </label>
            {/* React - Build out forms to handle when new users want to sign up, first name, last name, email and password */}
            <button onClick={submit}>Sign Up</button>
         </div>
+        </form>
     )
 }
 
