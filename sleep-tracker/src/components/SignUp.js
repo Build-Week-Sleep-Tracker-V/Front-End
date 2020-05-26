3d67fdd175bfd13d176517d853078b5e33843a33
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-05-26 15:08:00 -0700

    form validation added

--- sleep-tracker/src/components/SignUp.js
+++ sleep-tracker/src/components/SignUp.js
@@ -1,21 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {useHistory} from 'react-router-dom';
+import formSchema from './formSchema'
+import * as yup from 'yup'
 
+const initialFormErrors= {
+    userName: '',
+    password: '',
+    verify: ''
+  }
 const SignUp = () => {
     const history = useHistory()
+    const [disabled, setDisabled] = useState(true)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
     const [signUpData, setSignUpData] = useState({
         userName:'',
         password: '',
-        verify: ''
+        verify: '',
         // React - fill in properties needed for log in form that will be used for state management //
     })
 
     const onInputChange = evt=>{
         const [name] = evt.target
         const[value] = evt.target
+        yup
+        .reach(formSchema, name)
+        .validate(value)
+        .then(valid =>{
+          setFormErrors({
+            ...formErrors, [name]: ''
+          })
+        })
+        .catch(err=>{
+          setFormErrors({
+            ...formErrors,
+            [name]: err.errors[0]
+          })
+        })
         setSignUpData({...signUpData, [name]: value})
     }
+
+    
 
 
     const submit = e =>{ 
@@ -32,23 +57,30 @@ const SignUp = () => {
                 console.log(err)
             })
     }
+    useEffect(()=>{
+        formSchema.isValid(signUpData)
+          .then(valid =>{
+            setDisabled(!valid)
+          })
+        }, [signUpData])
+
     return(
        <form>
-       <div>
-            <h1>Sign Up</h1>
-            <h1>Login</h1>
-                <label>Username:
-                    <input type='text' placeholder='Create a username' maxLength='100' name='userName' value={signUpData.userName} onChange={onInputChange} />
-                </label>
-                <label>Password:
-                    <input type='text' placeholder='Create a password' maxLength='100' name='password' value={signUpData.password} onChange={onInputChange} />
-                </label>
-                <label>Verify password:
-                    <input type='text' placeholder='Retype your password' maxLength='100' name='verify' value={signUpData.verify} onChange={onInputChange} />
-                </label>
-           {/* React - Build out forms to handle when new users want to sign up, first name, last name, email and password */}
-           <button onClick={submit}>Sign Up</button>
-        </div>
+        <div>
+                <h1>Sign Up</h1>
+                <h1>Login</h1>
+                    <label>Username:
+                        <input type='text' placeholder='Create a username' maxLength='100' name='userName' value={signUpData.userName} onChange={onInputChange} />
+                    </label>
+                    <label>Password:
+                        <input type='text' placeholder='Create a password' maxLength='100' name='password' value={signUpData.password} onChange={onInputChange} />
+                    </label>
+                    <label>Verify password:
+                        <input type='text' placeholder='Retype your password' maxLength='100' name='verify' value={signUpData.verify} onChange={onInputChange} />
+                    </label>
+            {/* React - Build out forms to handle when new users want to sign up, first name, last name, email and password */}
+            <button disabled = {disabled} onClick={submit}>Sign Up</button>
+            </div>
         </form>
     )
 }
